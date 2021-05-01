841f819c2f14e204f16516b39e9937a59c704281
Fahmi Derbali <fahmiderbali@gmail.com>
2021-05-01 09:07:30 -0400

    Finaliser le rapport des voitures

--- Controllers/VoitureController.cs
+++ Controllers/VoitureController.cs
@@ -21,6 +21,60 @@ namespace Labo1.Controllers
             return View();
         }
 
+        // GET: Voiture/Rapport
+        public ActionResult Rapport()
+        {
+            var listVoitures = new List<Models.Voiture> { };
+            if (System.IO.File.Exists(Server.MapPath(@"~/App_Data/voiture.txt"))){
+                System.IO.StreamReader ficVoitures = new System.IO.StreamReader(Server.MapPath(@"~/App_Data/voiture.txt"));
+                var ligne = "";
+                var delimiteur = ':';
+                Double TotalDesVentes = 0;
+                Int16 NbVoitures = 0;
+                Int16 NbVoituresAmericainesDePlusDe2000 = 0;
+                Int16 NbVoituresRecentes = 0;
+                Int16 NbVoituresJaponaise = 0;
+                Double PrixVoituresJaponaise = 0;
+                Double PrixAmericaineLaMoinsChere = 99999999;
+                String ImmatriculationVoitureAmericaineLaMoinsChere = "";
+                while ((ligne = ficVoitures.ReadLine()) != null)
+                {
+                    var attributs = ligne.Split(delimiteur);
+                    var uneVoiture = new Models.Voiture() { Numero = attributs[0],  Sorte = Convert.ToInt16(attributs[1]), Annee = attributs[2], Prix = Convert.ToDouble(attributs[3]) };
+                    listVoitures.Add(uneVoiture);
+                    NbVoitures++;
+                    TotalDesVentes += uneVoiture.Prix;
+                    if(uneVoiture.Sorte == 1 && uneVoiture.Prix > 2000)
+                    {
+                        NbVoituresAmericainesDePlusDe2000++;
+                    }
+                    if( Convert.ToInt16(uneVoiture.Annee) >= 1991)
+                    {
+                        NbVoituresRecentes++;
+                    }
+                    if (uneVoiture.Sorte == 2)
+                    {
+                        NbVoituresJaponaise++;
+                        PrixVoituresJaponaise += uneVoiture.Prix;
+                    }
+                    if (uneVoiture.Sorte == 2 && uneVoiture.Prix < PrixAmericaineLaMoinsChere)
+                    {
+                        PrixAmericaineLaMoinsChere = uneVoiture.Prix;
+                        ImmatriculationVoitureAmericaineLaMoinsChere = uneVoiture.Numero;
+                    }
+
+                }
+                ViewData["NbVoitures"] = NbVoitures;
+                ViewData["TotalDesVentes"] = TotalDesVentes;
+                ViewData["NbVoituresAmericainesDePlusDe2000"] = NbVoituresAmericainesDePlusDe2000;
+                ViewData["NbVoituresRecentes"] = NbVoituresRecentes;
+                ViewData["PrixMoyenVoitureJaponaise"] = PrixVoituresJaponaise / NbVoituresJaponaise;
+                ViewData["PrixAmericaineLaMoinsChere"] = PrixAmericaineLaMoinsChere;
+                ViewData["ImmatriculationVoitureAmericaineLaMoinsChere"] = ImmatriculationVoitureAmericaineLaMoinsChere;
+            }
+            return View(listVoitures);
+        }
+
         // POST: Voiture/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
