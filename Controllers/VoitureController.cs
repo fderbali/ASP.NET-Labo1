5d6fc71072210e13a8128b361224ec34c5b58736
Fahmi Derbali <fahmiderbali@gmail.com>
2021-05-01 06:23:15 -0400

    Ajoutez des fichiers projet.

---
+++ Controllers/VoitureController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Labo1.Controllers
+{
+    public class VoitureController : Controller
+    {
+        // GET: Voiture
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Voiture/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Voiture/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            String ligne = "";
+            try
+            {
+                using (StreamWriter ficVoiture = new StreamWriter(Server.MapPath(@"~/App_Data/voiture.txt"), true))
+                {
+                    ligne += Convert.ToString(collection["Numero"]) + ":";
+                    ligne += Convert.ToString(collection["Sorte"]) + ":";
+                    ligne += Convert.ToString(collection["Annee"]) + ":";
+                    ligne += Convert.ToString(collection["Prix"]) ;
+                    ficVoiture.WriteLine(ligne);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Voiture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Labo1.Models
+{
+    public class Voiture
+    {
+        public string Numero { get; set; }
+        public int Sorte { get; set; }
+        public string Annee { get; set; }
+        public double Prix { get; set; }
+    }
+}
